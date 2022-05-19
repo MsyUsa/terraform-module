d3298ee628ae797fc37fbfd3abcbfe682537bc6e
MSY <msaidyilmazusa@gmail.com>
2022-05-18 20:15:03 -0400

    Completed Modules

--- ROOT/app-server/main.tf
+++ ROOT/app-server/main.tf
@@ -8,6 +8,6 @@ module "app-vpc" {
 
 module "app-server" {
   source = "git::https://github.com/MsyUsa/terraform-module.git//CHILD/EC2"
-  subnet_id = module.app-vpc.private_subnet_id
+  subnet_id = module.app-vpc.public_subnet_id
 
 }
